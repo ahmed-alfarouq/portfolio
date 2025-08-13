5dd2ce43bf14a5b2a2d6af7247ebc5f865ad55de
ahmed-al-farouq <mediocre23534@gmail.com>
2025-08-13 16:10:40 +0300

    Enhance error handling in sendMail function to catch and throw errors with descriptive messages.

--- src/services/sendMail.ts
+++ src/services/sendMail.ts
@@ -3,22 +3,22 @@ import type { SendMail } from "@/types";
 const API = import.meta.env.VITE_API_URL as string;
 
 const sendMail = async ({ name, email, subject, message }: SendMail) => {
-  const response = await fetch(`${API}/api/send-email`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify({ name, email, subject, message }),
-  });
+  try {
+    const response = await fetch(`${API}/api/send-email`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ name, email, subject, message }),
+    });
 
-  if (!response.ok) {
-    throw new Error("Something went wrong. Please try again later.");
+    const result = await response.json();
+
+    return result;
+  } catch (error) {
+    throw Error((error as Error).message);
   }
-
-  const result = await response.json();
-
-  return result;
 };
 
 export default sendMail;
